9554ac3b5a141733f5cec28a9376fbfd09707a0e
Jaime_Fame_109 <1261360793@qq.com>
2023-01-10 16:50:57 +0800

    使用Jinja模板处理字符串

---
+++ Jinja2_/use_Jinja2_.py
@@ -0,0 +1,20 @@
+from jinja2 import Template
+
+_MOVIES_TMPL = '''\
+Welcome, {{username}}.
+{%for name, rating in movies %}
+* {{ name }}, Rating: {{ rating|default("[NOT RATED]", True) }}
+{%- endfor %}
+'''
+
+def render_movies_j2(username, movies):
+    tmpl = Template(_MOVIES_TMPL)
+    return tmpl.render(username=username, movies=movies)
+
+movies = [
+    ('The Shawshank Redemption', '9.3'),
+    ('The Prestige', '8.5'),
+    ('Mulan', None),
+]
+
+print(render_movies_j2('piglei', movies))
