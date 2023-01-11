58085012fcdf36dbca8624e15d004eb525805d83
Jaime_Fame_109 <1261360793@qq.com>
2023-01-11 12:35:28 +0800

    集合运算：求交集，并集和差集

---
+++ set_operations/difference_.py
@@ -0,0 +1,8 @@
+fruits_1 = {'apple', 'orange', 'pineapple'}
+fruits_2 = {'tomato', 'orange', 'grapes', 'mango'}
+
+print(fruits_1 - fruits_2)
+print(fruits_1.difference(fruits_2))
+
+print(fruits_2 - fruits_1)
+print(fruits_2.difference(fruits_1))
---
+++ set_operations/intersection_.py
@@ -0,0 +1,5 @@
+fruits_1 = {'apple', 'orange', 'pineapple'}
+fruits_2 = {'tomato', 'orange', 'grapes', 'mango'}
+
+print(fruits_1 & fruits_2)
+print(fruits_1.intersection(fruits_2))
