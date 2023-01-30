71e6e7e448852300fd780f4ade5e09c36c4dd2dc
Jaime_Fame_109 <1261360793@qq.com>
2023-01-30 11:29:26 +0800

    如果一个类实现了 __call__ 魔法方法，那么它的实例也会变成可调用对象

--- decorator_/class__call__.py
+++ decorator_/class__call__.py
@@ -1,7 +1,7 @@
 class Foo:
-    def __call__(self,name):
+    def __call__(self,name): # __call__ 魔法方法是用来实现可调用对象的关键方法
         print(f'Hello,{name}')
 
 foo = Foo()
 print(callable(foo))
-foo('xx')
+foo('xx') # 调用类实例时，可以像调用普通函数一样提供额外参数
